102fe0cb7b2767665055cd52a68711a3f1afcbe4
wen <wen@trackview.co>
2021-10-08 11:40:02 +0800

    remove dependence on aria2c
    TEST: n/a

--- install_dep.sh
+++ install_dep.sh
@@ -1,5 +1,5 @@
 if [ -f "pdfbox-app-3.0.0-alpha2.jar" ]; then
     echo "pdfbox existed"
 else
-    aria2c 'https://dlcdn.apache.org/pdfbox/3.0.0-alpha2/pdfbox-app-3.0.0-alpha2.jar'
+    curl -o 'pdfbox-app-3.0.0-alpha2.jar' 'https://dlcdn.apache.org/pdfbox/3.0.0-alpha2/pdfbox-app-3.0.0-alpha2.jar'
 fi
